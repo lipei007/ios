525dfd920069d3f47b259cd0686f51bc23f41762
lipei007 <emerysyy@gmail.com>
2016-05-05 23:28:30 +0800

    总结

---
+++ JKUtilDemo/JKUtilDemo/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  JKUtilDemo
+//
+//  Created by emerys on 16/4/20.
+//  Copyright © 2016年 Emerys. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "JKUtil.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor purpleColor];
+    
+    JKCacheSlider *cacheSlider = [JKCacheSlider cacheSliderWithFrame:CGRectMake(0, 100, JK_ScreenWidth, 10)];
+    cacheSlider.cacheValue = 0.5;
+    
+    [self.view addSubview:cacheSlider];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+//    JKMoviePlayerPortraitViewController *player = [[JKMoviePlayerPortraitViewController alloc] init];
+//    
+//    [self presentViewController:player animated:YES completion:nil];
+}
+
+@end
