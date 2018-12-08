cc7be398b9ce3a35e8a66e4e554e2bead7e4d9ff
lipei007 <macmini1@mini.local>
2018-12-08 16:21:46 +0800

    添加UIImage方向修正

---
+++ JKUtilDemo/JKUtil/Category/UIImage/UIImage+Orientation/UIImage+JKOrientation.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+JKOrientation.h
+//  JKUtilDemo
+//
+//  Created by Jack on 2018/12/8.
+//  Copyright © 2018年 Emerys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (JKOrientation)
+
+- (UIImage *)jk_fixOrientation;
+
+@end
